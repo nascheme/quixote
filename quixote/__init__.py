c40fbfc4332b899d021d0c447112f8393dcfa642
Neil Schemenauer <nas@arctrix.com>
2014-01-17 15:27:42 -0600

    Update version number to 2.8.

--- quixote/__init__.py
+++ quixote/__init__.py
@@ -3,7 +3,7 @@
 A small and flexible Python web application framework.
 """
 
-__version__ = '2.8b2'
+__version__ = '2.8'
 
 # These are frequently needed by Quixote applications.
 from quixote.publish import \
