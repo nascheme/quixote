46d3046d54269fb0cec8b2a41473301c858ad2b4
Neil Schemenauer <nas@arctrix.com>
2017-10-25 13:30:38 -0700

    Add --app argument to server scripts.
    If provided, --factory becomes <app>.<factory>.  Change the default
    factory argument to 'create_publisher'.  If the factory contains a dot
    then it is assumed to be a complete path with the package name and
    is used as-is, rather than using 'app' as a prefix.

--- quixote/server/util.py
+++ quixote/server/util.py
@@ -4,8 +4,18 @@
 from optparse import OptionParser
 from quixote.util import import_object
 
+
+class Parser(OptionParser):
+    def parse_args(self, *args, **kwargs):
+        options, values = OptionParser.parse_args(self, *args, **kwargs)
+        if options.factory and '.' not in options.factory:
+            # make factory include package name as well
+            options.factory = options.app + '.' + options.factory
+        return options, values
+
+
 def get_server_parser(doc):
-    parser = OptionParser()
+    parser = Parser()
     parser.set_description(doc)
     default_host = 'localhost'
     parser.add_option(
@@ -15,12 +25,19 @@ def get_server_parser(doc):
     parser.add_option(
         '--port', dest="port", default=default_port, type="int",
         help="Port to listen on. (default=%s)" % default_port)
-    default_factory = 'quixote.demo.create_publisher'
+    default_factory = 'create_publisher'
     parser.add_option(
         '--factory', dest="factory",
         default=default_factory,
         help="Path to factory function to create the site Publisher. "
+             "If it does not contain a dot, it is relative to app package. "
              "(default=%s)" % default_factory)
+    default_app = 'quixote.demo'
+    parser.add_option(
+        '--app', dest="app",
+        default=default_app,
+        help="Name of package that contains Quixote application. "
+             "(default=%s)" % default_app)
     return parser
 
 def main(run):
