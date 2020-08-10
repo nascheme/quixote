73fc4f41ee870ac008b2fa386e1dcdcd952370f1
Neil Schemenauer <nas@arctrix.com>
2020-08-10 15:05:57 -0700

    Update version to 3.2.

--- quixote/__init__.py
+++ quixote/__init__.py
@@ -3,7 +3,7 @@
 A small and flexible Python web application framework.
 """
 
-__version__ = '3.2a2'
+__version__ = '3.2'
 
 # These are frequently needed by Quixote applications.
 from quixote.publish import \
